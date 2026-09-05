f048d0113d83484df2e89b12fc2e63c0890a50e0
Brennan Douglas <brennan@kratos.lan.brennandouglas.com>
2026-09-05 19:44:55 -0400

    release: prepare 2026.9.5-fork.4

--- custom_components/voice_satellite/const.py
+++ custom_components/voice_satellite/const.py
@@ -14,7 +14,7 @@ CONVERSATION_TRANSPORT_HOME_ASSISTANT: Final[str] = "Home Assistant"
 CONVERSATION_TRANSPORT_EXTERNAL: Final[str] = "External"
 
 # Version - synced from package.json by scripts/sync-version.js
-INTEGRATION_VERSION: str = "2026.9.5-fork.3"
+INTEGRATION_VERSION: str = "2026.9.5-fork.4"
 
 # Bus events fired for user automations
 EVENT_TIMER: Final[str] = "voice_satellite_timer"
